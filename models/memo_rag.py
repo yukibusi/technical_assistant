caffd012d242765e1de50f5657a78cb0b9cd18be
dmwrrr1 <Dong-ming.Wu@jp.ricoh.com>
2024-06-18 04:32:41 +0000

    add formatted memo to DB from dir

--- models/memo_rag.py
+++ models/memo_rag.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import bs4
 from langchain import hub
 from langchain_chroma import Chroma
@@ -8,34 +10,27 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from dotenv import dotenv_values
 
-OPENAI_API_KEY=str(dotenv_values()["OPENAI_API_KEY"])
-embeddings = OpenAIEmbeddings(
+database_dir = "../memo_database"
+add_docs_from_formatted_memos = False
+formatted_memos_dir = "../formatted_memos"
+
+#OPENAI_API_KEY=str(dotenv_values()["OPENAI_API_KEY"])
+embedding = OpenAIEmbeddings(
     model="text-embedding-3-large",
-    openai_api_key=OPENAI_API_KEY
+    #openai_api_key=OPENAI_API_KEY
 )
 
-# Load, chunk and index the contents of the blog.
-loader = TextLoader("test")
-docs = loader.load()
+vectorstore = Chroma(persist_directory=database_dir, embedding_function=embedding)
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-splits = text_splitter.split_documents(docs)
-vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+if add_docs_from_formatted_memos:
+    for path in glob.glob(formatted_memos_dir + '/*'):
+        with open(path) as f:
+            doc = f.read()
+        vectorstore._collection.add(
+            ids=[str(vectorstore._collection.count()+1)],
+            embeddings=[embedding.embed_query(doc)],
+            metadatas=[{"key": "value"}],
+            documents=[doc]
+        )
 
-# Retrieve and generate using the relevant snippets of the blog.
 retriever = vectorstore.as_retriever()
-prompt = hub.pull("rlm/rag-prompt")
-
-
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
-
-rag_chain.invoke("What is Task Decomposition?")
