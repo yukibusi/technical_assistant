ae7765bda556764e830aab8a812977b09598ad89
dmwrrr1 <Dong-ming.Wu@jp.ricoh.com>
2024-06-18 06:24:14 +0000

    同じファイル名のtxtをdbに重複して追加しないように修正した

--- models/memo_rag.py
+++ models/memo_rag.py
@@ -1,6 +1,7 @@
 import os
 import glob
-import bs4
+import pickle
+#import bs4
 from langchain import hub
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import WebBaseLoader
@@ -10,9 +11,17 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from dotenv import dotenv_values
 
+#この四つは変えて大丈夫
 database_dir = "../memo_database"
-add_docs_from_formatted_memos = False
-formatted_memos_dir = "../formatted_memos"
+add_docs_from_formatted_memos = True
+formatted_memos_dir = "../error_file_modify"
+added_memo_file_names_dir = "../added_memo_file_names"
+
+if len(os.listdir(added_memo_file_names_dir)) == 0:
+    added_memo_file_names = set()
+else:
+    with open(added_memo_file_names_dir + '/added_memo_file_names.pickle', mode='rb') as f:
+        added_memo_file_names = pickle.load(f)
 
 #OPENAI_API_KEY=str(dotenv_values()["OPENAI_API_KEY"])
 embedding = OpenAIEmbeddings(
@@ -23,14 +32,23 @@ embedding = OpenAIEmbeddings(
 vectorstore = Chroma(persist_directory=database_dir, embedding_function=embedding)
 
 if add_docs_from_formatted_memos:
-    for path in glob.glob(formatted_memos_dir + '/*'):
-        with open(path) as f:
-            doc = f.read()
-        vectorstore._collection.add(
-            ids=[str(vectorstore._collection.count()+1)],
-            embeddings=[embedding.embed_query(doc)],
-            metadatas=[{"key": "value"}],
-            documents=[doc]
-        )
+    for path in glob.glob(formatted_memos_dir + '/*.txt'):
+        filename = path.split("/")[-1]
+        if filename not in added_memo_file_names:
+            with open(path) as f:
+                doc = f.read()
+            vectorstore._collection.add(
+                ids=[str(vectorstore._collection.count()+1)],
+                embeddings=[embedding.embed_query(doc)],
+                metadatas=[{"filename": filename}],
+                documents=[doc]
+            )
+            added_memo_file_names.add(filename)
 
-retriever = vectorstore.as_retriever()
+with open(added_memo_file_names_dir + '/added_memo_file_names.pickle', mode='wb') as f:
+    pickle.dump(added_memo_file_names, f)
+
+print(vectorstore._collection.count())
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 1})
+retrieved_docs = retriever.invoke('TypeError: can only concatenate list (not "str") to list')
+print(retrieved_docs[0].page_content)
