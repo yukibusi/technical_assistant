95f4d01fb2d7b377be68b1d3709e0013cdc302b9
ShokoGenda <Shoko.Genda@jp.ricoh.com>
2024-06-18 05:10:34 +0000

    update assist_model.py for web application

--- models/assist_model.py
+++ models/assist_model.py
@@ -1,29 +1,39 @@
 import bs4
 from dotenv import dotenv_values
+from langchain import hub
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 
-from memo_rag import retriever, vectorstore, prompt
+from memo_rag import retriever, vectorstore
 
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
+def main(error):
+    prompt = hub.pull("rlm/rag-prompt")
 
-OPENAI_API_KEY = dotenv_values()["OPENAI_API_KEY"]
+    OPENAI_API_KEY = dotenv_values()["OPENAI_API_KEY"]
 
-llm = ChatOpenAI(
-    model="gpt-4o",
-    api_key=OPENAI_API_KEY,
-    # temperature=0,
-)
+    llm = ChatOpenAI(
+        model="gpt-4o",
+        api_key=OPENAI_API_KEY,
+        # temperature=0,
+    )
 
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    
+    assist = rag_chain.invoke(f"以下のエラーの解決方法を教えてください。エラー:{error}")
+    
+    return assist
 
-print(rag_chain.invoke(f"入力されたエラーの解決方法を表示してください\n{input("エラー：")}"))
+if __name__ == "__main__":
+    error = input("解決したいエラーを入力してください：")
+    assist = main(error)
+    print(assist)
